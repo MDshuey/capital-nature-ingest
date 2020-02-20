ccec29155b1471c3094976bd3c9d3628e7a2dd6e
MD Shuey <44530405+MDshuey@users.noreply.github.com>
2020-02-20 03:59:23 -0500

    Update ecc.py

--- events/ecc.py
+++ events/ecc.py
@@ -1,4 +1,4 @@
 
 #MD Shuey
 #Earth Convservation Corps scrape
-#As of 2/20/20 their website is still empty, so no events. Undergoing a change in leadership according to the website, so there's no rush
+#As of 2/20/20 their calendar is still empty, so no events (undergoing a change in leadership according to the website). For now, no rush
